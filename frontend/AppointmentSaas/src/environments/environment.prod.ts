9ef2eff2b87fde5f65c013fc7fb27ef8b463e3c6
sudipto21in2024 <sudipto21in2024@outlook.com>
2025-09-19 20:30:14 +0530

    FE-001-03: Setup development tools and environment for frontend/AppointmentSaas

---
+++ frontend/AppointmentSaas/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_BASE_URL: '/api',
+  DEBUG_MODE: false,
+};
---
+++ frontend/AppointmentSaas/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_BASE_URL: 'http://localhost:3000/api',
+  DEBUG_MODE: true,
+};
