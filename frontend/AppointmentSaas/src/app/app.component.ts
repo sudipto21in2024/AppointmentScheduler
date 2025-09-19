f7ccfbd6e619f66f693b7de6f8435d21d074c382
sudipto21in2024 <sudipto21in2024@outlook.com>
2025-09-19 18:45:19 +0530

    FE-001-01: Setup Angular Project Structure

---
+++ frontend/AppointmentSaas/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  protected readonly title = signal('AppointmentSaas');
+}
